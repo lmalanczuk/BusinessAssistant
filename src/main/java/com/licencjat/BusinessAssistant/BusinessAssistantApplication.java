1b5d2eb9525b7706d807892abc76f1b08d2067fa
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2025-01-29 10:17:31 +0100

    init

---
+++ src/main/java/com/licencjat/BusinessAssistant/BusinessAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.licencjat.BusinessAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licencjat/BusinessAssistant/BusinessAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licencjat.BusinessAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
