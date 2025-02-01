1e7d63b36da76b7c9dadea7811692a6cfa67eb72
m-okuniewicz <m.okuniewicz@wasko.pl>
2025-02-01 17:12:18 +0100

    Sidebar

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {CalendarComponent} from "./calendar/calendar.component";
+import {TranscriptionsComponent} from "./transcriptions/transcriptions.component";
+import {SettingsComponent} from "./settings/settings.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'transcriptions', component: TranscriptionsComponent},
+  {path: 'settings', component: SettingsComponent}
+];
---
+++ frontend/frontend-app/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,45 @@
+import {Component, input, output} from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {NgClass} from "@angular/common";
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterModule, NgClass],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'Dashboard',
+    },
+    {
+      routeLink: 'calendar',
+      icon: 'fal fa-calendar',
+      label: 'Calendar',
+    },
+    {
+      routeLink: 'transcriptions',
+      icon: 'fal fa-pen',
+      label: 'Transcriptions',
+    },
+    {
+      routeLink: 'settings',
+      icon: 'fal fa-cog',
+      label: 'Settings',
+    },
+  ];
+
+  toggleCollapse(): void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
+}
